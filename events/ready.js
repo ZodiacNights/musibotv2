d8f2bc6f10280b2bd106b6462db1f6cb95df7c54
ZodiacNights <79066091+ZodiacNights@users.noreply.github.com>
2021-03-06 16:41:44 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,7 @@
 
+module.exports = async (client) => {
+    console.log(`[API] Logged in as ${client.user.username}`);
+    await client.user.setActivity("NanixPrada", {
+      type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    });
+  };
